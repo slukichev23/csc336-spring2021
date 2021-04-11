acd1df8fdb3ddb6baad8c7ba65c60f1ec995285e
Semyon Lukichev <semyon.lukichev@gmail.com>
2021-04-11 18:57:29 -0400

    finished tic tac toe game

--- creative_project_2/cp_2.js
+++ creative_project_2/cp_2.js
@@ -1,18 +1,20 @@
 (function() {
 
   "use strict";
-  // Statistics which can be viewed in stats-view
+  // Statistics which can be viewed in stats-view. Will not be reset by restart button.
   let gamesPlayed = 0;
   let wins = 0;
   let losses = 0;
   let winPercent = 0;
+  let timeArray = [];
   let avgTime = 0; // in seconds
-  // Keeps track for game itself
+  // Keeps track for game itself. These will be reset aftereach game.
   let clickCount = 0;
-  let fillValues = false;
   let tileValues = [0, 0, 0, 0, 0, 0, 0, 0, 0]; // 0 = empty, 1 = player, 2 = computer
   let timerID = null;
   let time = 0;
+  let playerCanMove = true; // player will always have first move
+  let gameBoardMade = false;
 
   window.addEventListener("load", init);
 
@@ -21,27 +23,21 @@
     const startBtn = id("play-btn");
     // ADD: rulesBtn, statsBtn, returnToGameBtn
     startBtn.addEventListener("click", startGame);
+    id("return-to-game").addEventListener("click", startGame);
   }
 
     // Event listener for Start Button
     function startGame(){
-      // 1. Add hidden class to Rules View
+      // 1. Add hidden class to Rules View and Stats View
       id("rules-view").classList.add("hidden");
+      id("stats-view").classList.add("hidden");
       // 2. Remove hidden class from Game View
       let gameView = id("game-view");
       gameView.classList.remove("hidden");
-
-      if (id("game-board")){
-        // game-board is already made
-        // user checked rules and wants to return to the game they had going.
-        // setting flag to fill in values of tiles after generation
-        fillValues = true;
-      } else {
-        // TODO: Start timer
-        timerID = setInterval(updateCountTimer, 1000);
-      }
+      console.log(gameBoardMade);
       // 3. Create div with id "game-board" and append it to Game View
       let gameBoard = gen("div");
+      console.log(gameBoardMade);
       gameBoard.id  = "game-board";
       gameView.appendChild(gameBoard);
       // 4. Add class show-border to Game Board
@@ -56,6 +52,37 @@
         newTile.addEventListener("click", tileClickEvent);
         gameBoard.appendChild(newTile);
       }
+      if (gameBoardMade){
+        console.log("game-board is already made, adding saved images");
+        // user checked rules and wants to return to the game they had going.
+        // fillValues = true;
+        let board = qsa(".game-tile");
+        console.log(board);
+        for (let i = 0; i < board.length; i++) {
+          if (tileValues[i] == 1) {
+            console.log("Tile " + i + " has value of 1");
+            // add player icon
+            let playerImg = gen("img");
+            playerImg.src = "images/player.png";
+            board[i].appendChild(playerImg);
+          } else if (tileValues[i] == 2) {
+            console.log("Tile " + i + " has value of 2");
+            // add computer icon
+            let compImg = gen("img");
+            compImg.src = "images/computer.png";
+            board[i].appendChild(compImg);
+          } else {
+            console.log("Tile " + i + " has value of 0");
+          }
+        }
+      } else {
+        // TODO: Start timer
+        if (timerID == null){
+          timerID = setInterval(updateCountTimer, 1000);
+        }
+
+      }
+      gameBoardMade = true;
       // 7. Create "Rules", "Stats", and "Restart Buttons" and append them to Game View
       let rulesBtn = gen("button");
       rulesBtn.id = "rules-btn";
@@ -65,7 +92,7 @@
       resetboardBtn.innerHTML = "Restart";
       let statsBtn = gen("button");
       statsBtn.id = "stats-btn";
-      statsBtn.innerHTML = "Your stats";
+      statsBtn.innerHTML = "Stats";
       let buttonsContainer = gen("div");
       buttonsContainer.id = "container";
       gameView.appendChild(buttonsContainer);
@@ -74,35 +101,303 @@
       buttonsContainer.appendChild(statsBtn);
 
       // 8. If flag to fill was set earlier, fill board with saved values:
-      // function goes here
+      // ### function goes here ###
 
       // 9. Add event handlers to each button
-      // Rules handler:
-      // Removes Game Board children and then Game Board itself
-      // Adds hidden class to Game View
-      // Removes hidden class from Rules View
       rulesBtn.addEventListener("click", rulesBtnHandler);
       resetboardBtn.addEventListener("click", resetboardBtnHandler);
+      statsBtn.addEventListener("click", statsBtnHandler);
 
     }
 
     function tileClickEvent() {
       console.log("tile index " + this.value + " has been clicked");
-      clickCount++;
       // 1. Check if this has child. If it does, either player or computer
       // has already clicked on it.
-      // 2. If empty, gen img and append as child. Set img to player's icon
-      // 3. If at least 3 clicks have been made, begin checking for winning condition
-      // Board:
+      console.log(this.children.length);
+      if (this.children.length < 1 && playerCanMove) {
+        clickCount++;
+        playerCanMove = false;
+        // 2. If empty, gen img and append as child. Set img to player's icon
+        let playerIcon = gen("img");
+        playerIcon.src = "images/player.png";
+        this.appendChild(playerIcon);
+        // Keeps track of board in array:
+        tileValues[this.value] = 1;
+        // 3. If at least 3 clicks have been made, begin checking for winning condition
+        if (clickCount >= 3) {
+          // Win condition check for player function goes here
+          if (checkWinCondition(1)) {
+            // Player wins.
+            console.log("Player won!");
+            playerCanMove = false;
+            clearInterval(timerID);
+            timerID = null;
+            // updating stats
+            gamesPlayed += 1;
+            wins += 1;
+            winPercent = wins / gamesPlayed;
+            timeArray.push(time);
+            let totalTimes = 0;
+            let count = 0;
+            for (let i = 0; i < timeArray.length; i++){
+              count++;
+              totalTimes += timeArray[i];
+            }
+            avgTime = totalTimes / count;
+            id("timer").innerHTML = "You won!";
+            updateStatsView();
+            return 1;
+          }
+        }
+        // If it is the player's 5th click, that means that the board is full.
+        if (clickCount >= 5) {
+          console.log("Draw!");
+          playerCanMove = false;
+          clearInterval(timerID);
+          timerID = null;
+          // updating stats
+          gamesPlayed += 1;
+          winPercent = wins / gamesPlayed;
+          timeArray.push(time);
+          let totalTimes = 0;
+          let count = 0;
+          for (let i = 0; i < timeArray.length; i++){
+            count++;
+            totalTimes += timeArray[i];
+          }
+          avgTime = totalTimes / count;
+          id("timer").innerHTML = "Draw!";
+          updateStatsView();
+          return 3;
+        }
+        // 5. Let computer pick where to go.
+        setTimeout(function(){
+          // ## Function that determines where computer will go ##
+          if (clickCount == 1){
+            randomComputerMove();
+          }
+          else {
+            // By this point player has gone at least 2 times.
+            // Check if there is a spot computer can go to win the round.
+            let possibleWinningTile = winningMoveExists(2);
+            if (possibleWinningTile >= 0) {
+              // There exists a spot where the computer can go and win the game.
+              addImageToTile(possibleWinningTile);
+              // Computer wins game.
+            } else {
+              // No immediate win path for computer.
+              // Checking if there is a defensive play it can make.
+              let possiblePlayerWin = winningMoveExists(1);
+              if (possiblePlayerWin >= 0) {
+                // There is a spot that the player can go to win the game next turn. Take it.
+                addImageToTile(possiblePlayerWin);
+              } else {
+                // No winning more nor defensive move detected, selecting random location!
+                console.log("No winning or defensive move detected, will choose a random location");
+                randomComputerMove();
+              }
+            }
+            // If not, check if there is a spot the player can go to win the round, and take it before they do
+
+            // Else, choose random spot
+          }
+          if (clickCount >= 3) {
+            // Win condition check for COMPUTER function goes here
+            if (checkWinCondition(2)) {
+              // Player wins.
+              console.log("Computer won!");
+              playerCanMove = false;
+              clearInterval(timerID);
+              timerID = null;
+              // updating stats
+              gamesPlayed += 1;
+              losses += 1;
+              winPercent = wins / gamesPlayed;
+              timeArray.push(time);
+              let totalTimes = 0;
+              let count = 0;
+              for (let i = 0; i < timeArray.length; i++){
+                count++;
+                totalTimes += timeArray[i];
+              }
+              avgTime = totalTimes / count;
+              id("timer").innerHTML = "You lost!";
+              updateStatsView();
+              return 2;
+            }
+
+          }
+          console.log("Tile Value array: " + tileValues);
+          playerCanMove = true;
+        }, 500);
+
+      }
+
+    } // End of tile chick event
+
+    function addImageToTile(index) {
+      // There exists a spot where the computer can go and win the game.
+      let gameTilesArray = qsa(".game-tile");
+      for (let i = 0; i < gameTilesArray.length; i++) {
+        if (gameTilesArray[i].value == index) {
+          let computerIcon = gen("img");
+          computerIcon.src = "images/computer.png";
+          gameTilesArray[i].appendChild(computerIcon);
+          // Update array
+          tileValues[index] = 2;
+        }
+      }
+    }
+
+    function randomComputerMove() {
+      let openIndexArray = [];
+      for (let i = 0; i < tileValues.length; i++){
+        if (tileValues[i] == 0) {
+          openIndexArray.push(i);
+          console.log(i + " is open");
+        }
+      }
+      // finds specifc tile with that value and adds computer icon to it
+      let computerMoveIndex = openIndexArray[random_amount(openIndexArray.length)];
+      let gameTilesArray = qsa(".game-tile");
+      for (let i = 0; i < gameTilesArray.length; i++) {
+        if (gameTilesArray[i].value == computerMoveIndex) {
+          let computerIcon = gen("img");
+          computerIcon.src = "images/computer.png";
+          gameTilesArray[i].appendChild(computerIcon);
+          // Update array
+          tileValues[computerMoveIndex] = 2;
+        }
+      }
+      // end
+    }
+
+    // for either player (1) or computer (2)
+    // if their next move can be a winning one, it will return the index of that tile.    // 0 1 2
+    // if it can't find a winning tile, returns -1                                        // 3 4 5
+    function winningMoveExists(player_or_computer) {                                      // 6 7 8
+      // checks rows
+      // Row 1
+      if (tileValues[0] == player_or_computer && tileValues[1] == player_or_computer && tileValues[2] == 0) {
+        return 2;
+      } else
+      if (tileValues[0] == player_or_computer && tileValues[1] == 0 && tileValues[2] == player_or_computer) {
+        return 1;
+      } else
+      if (tileValues[0] == 0 && tileValues[1] == player_or_computer && tileValues[2] == player_or_computer) {
+        return 0;
+      } else
+      // Row 2
+      if (tileValues[3] == player_or_computer && tileValues[4] == player_or_computer && tileValues[5] == 0) {
+        return 5;
+      } else
+      if (tileValues[3] == player_or_computer && tileValues[4] == 0 && tileValues[5] == player_or_computer) {
+        return 4;
+      } else
+      if (tileValues[3] == 0 && tileValues[4] == player_or_computer && tileValues[5] == player_or_computer) {
+        return 3;
+      } else
+      // Row 3
+      if (tileValues[6] == player_or_computer && tileValues[7] == player_or_computer && tileValues[8] == 0) {
+        return 8;
+      } else
+      if (tileValues[6] == player_or_computer && tileValues[7] == 0 && tileValues[8] == player_or_computer) {
+        return 7;
+      } else
+      if (tileValues[6] == 0 && tileValues[7] == player_or_computer && tileValues[8] == player_or_computer) {
+        return 6;
+      } else
+      // checks colums
+      // Column 1
+      if (tileValues[0] == player_or_computer && tileValues[3] == player_or_computer && tileValues[6] == 0) {
+        return 6;
+      } else
+      if (tileValues[0] == player_or_computer && tileValues[3] == 0 && tileValues[6] == player_or_computer) {
+        return 3;
+      } else
+      if (tileValues[0] == 0 && tileValues[3] == player_or_computer && tileValues[6] == player_or_computer) {
+        return 0;
+      } else
+      // Column 2
+      if (tileValues[1] == player_or_computer && tileValues[4] == player_or_computer && tileValues[7] == 0) {
+        return 7;
+      } else
+      if (tileValues[1] == player_or_computer && tileValues[4] == 0 && tileValues[7] == player_or_computer) {
+        return 4;
+      } else
+      if (tileValues[1] == 0 && tileValues[4] == player_or_computer && tileValues[7] == player_or_computer) {
+        return 1;
+      } else
+      // Column 3
+      if (tileValues[2] == player_or_computer && tileValues[5] == player_or_computer && tileValues[8] == 0) {
+        return 8;
+      } else
+      if (tileValues[2] == player_or_computer && tileValues[5] == 0 && tileValues[8] == player_or_computer) {
+        return 5;
+      } else
+      if (tileValues[2] == 0 && tileValues[5] == player_or_computer && tileValues[8] == player_or_computer) {
+        return 2;
+      } else
+      // checks diagonals
+      // Top Left to Bottom Right Diagonal
+      if (tileValues[0] == player_or_computer && tileValues[4] == player_or_computer && tileValues[8] == 0) {
+        return 8;
+      } else
+      if (tileValues[0] == player_or_computer && tileValues[4] == 0 && tileValues[8] == player_or_computer) {
+        return 4;
+      } else
+      if (tileValues[0] == 0 && tileValues[4] == player_or_computer && tileValues[8] == player_or_computer) {
+        return 0;
+      } else
+      // Top Right to Bottom Left Diagonal
+      if (tileValues[2] == player_or_computer && tileValues[4] == player_or_computer && tileValues[6] == 0) {
+        return 6;
+      } else
+      if (tileValues[2] == player_or_computer && tileValues[4] == 0 && tileValues[6] == player_or_computer) {
+        return 4;
+      } else
+      if (tileValues[2] == 0 && tileValues[4] == player_or_computer && tileValues[6] == player_or_computer) {
+        return 2;
+      } else {
+        return -1;
+      }
+    }
+
+    // returns true or false if input (player or computer) has won the game
+    function checkWinCondition(player_or_computer) {
       // 0 1 2
       // 3 4 5
       // 6 7 8
-      // 4. After player has clicked, set canPlayerMove = false (cooldown to let computer make its move)
-      // 5. Let computer pick where to go.
-        // a. If there is an immediate spot open that will cause player to win if they
-        // go there, take that spot. Will claim first of such spots that it detects.
-        // b. If there are no spots which will lead to immediate player victory, computer
-        // gets a list of open indexes and randomly chooses one of them.
+      // Rows
+      if (tileValues[0] == player_or_computer && tileValues[1] == player_or_computer && tileValues[2] == player_or_computer) {
+        return true;
+      } else
+      if (tileValues[3] == player_or_computer && tileValues[4] == player_or_computer && tileValues[5] == player_or_computer) {
+        return true;
+      } else
+      if (tileValues[6] == player_or_computer && tileValues[7] == player_or_computer && tileValues[8] == player_or_computer) {
+        return true;
+      } else
+      // columns
+      if (tileValues[0] == player_or_computer && tileValues[3] == player_or_computer && tileValues[6] == player_or_computer) {
+        return true;
+      } else
+      if (tileValues[1] == player_or_computer && tileValues[4] == player_or_computer && tileValues[7] == player_or_computer) {
+        return true;
+      } else
+      if (tileValues[2] == player_or_computer && tileValues[5] == player_or_computer && tileValues[8] == player_or_computer) {
+        return true;
+      } else
+      // diagonals
+      if (tileValues[0] == player_or_computer && tileValues[4] == player_or_computer && tileValues[8] == player_or_computer) {
+        return true;
+      } else
+      if (tileValues[2] == player_or_computer && tileValues[4] == player_or_computer && tileValues[6] == player_or_computer) {
+        return true;
+      } else {
+        return false;
+      }
     }
 
     function removeGameViewChildren(button) {
@@ -113,8 +408,11 @@
       gameViewParent.removeChild(buttonContainerParent);
     }
 
+    // Rules handler:
+    // Removes Game Board children and then Game Board itself
+    // Adds hidden class to Game View
+    // Removes hidden class from Rules View
     function rulesBtnHandler() {
-      // Removes children from game-view
       removeGameViewChildren(this);
       let gameViewParent = id("game-view");
       gameViewParent.classList.add("hidden");
@@ -122,25 +420,37 @@
     }
 
     function resetboardBtnHandler() {
-      // Removes children from game-view
+
       removeGameViewChildren(this);
-      fillValues = false;
+      // fillValues = false;
+      gameBoardMade = false;
       time = 0;
       clearInterval(timerID);
       timerID = null;
+      clickCount = 0;
+      tileValues = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+      playerCanMove = true;
+      id("timer").innerHTML = "Time: 00:00";
+
       startGame();
     }
 
     function statsBtnHandler() {
-      // Removes children from game-view
+
       removeGameViewChildren(this);
       let gameViewParent = id("game-view");
       gameViewParent.classList.add("hidden");
       id("stats-view").classList.remove("hidden");
+
+      // adds return to game button
+      // let returnToGameBtn = gen("button");
+      // returnToGameBtn.id = "return-to-game";
+      // returnToGameBtn.innerHTML = "Return";
+      // id("stats-view").appendChild(returnToGameBtn);
+
     }
 
     function updateCountTimer() {
-      // TODO: Add hour count
       time += 1;
       let minutes = 0;
       let seconds = 0;
@@ -158,7 +468,15 @@
       } else {
         strSeconds = seconds.toString();
       }
-      id("timer").innerText = strMinutes + ":" + strSeconds;
+      id("timer").innerText = "Time: " + strMinutes + ":" + strSeconds;
+    }
+
+    function updateStatsView() {
+      id("games-played").innerHTML = gamesPlayed.toString();
+      id("wins").innerHTML = wins.toString();
+      id("losses").innerHTML = losses.toString();
+      id("win-percentage").innerHTML = winPercent.toString() + "%";
+      id("avg-time").innerHTML = avgTime.toString() + " seconds";
     }
 
 
